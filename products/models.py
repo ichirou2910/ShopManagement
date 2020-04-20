a8f256d68dced0fbb9cb186776f5fa35f80499aa
ichirou2910 <ichiroukeita2910@gmail.com>
2020-04-20 12:34:08 +0700

    Connect to django database

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    productid = models.CharField(db_column='productID', primary_key=True, max_length=10)  # Field name made lowercase.
+    productname = models.CharField(db_column='productName', max_length=25)  # Field name made lowercase.
+    productimage = models.CharField(db_column='productImage', max_length=100)  # Field name made lowercase.
+    company = models.CharField(max_length=25)
+    productdescription = models.TextField(db_column='productDescription')  # Field name made lowercase.
+    quantityinstock = models.SmallIntegerField(db_column='quantityInStock')  # Field name made lowercase.
+    sellprice = models.IntegerField(db_column='sellPrice')  # Field name made lowercase.
+    buyprice = models.IntegerField(db_column='buyPrice')  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'products'
