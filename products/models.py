9f4488542cac784ede79c6377bf961f36cdc7cc5
ichirou2910 <ichiroukeita2910@gmail.com>
2020-04-20 16:38:45 +0700

    Add details page for each product

--- products/models.py
+++ products/models.py
@@ -8,15 +8,15 @@
 from django.db import models
 
 
-class Products(models.Model):
-    productid = models.CharField(db_column='productID', primary_key=True, max_length=10)  # Field name made lowercase.
-    productname = models.CharField(db_column='productName', max_length=25)  # Field name made lowercase.
-    productimage = models.CharField(db_column='productImage', max_length=100)  # Field name made lowercase.
+class Product(models.Model):
+    product_id = models.CharField(db_column='productID', primary_key=True, max_length=10)  # Field name made lowercase.
+    product_name = models.CharField(db_column='productName', max_length=25)  # Field name made lowercase.
+    product_image = models.CharField(db_column='productImage', max_length=100)  # Field name made lowercase.
     company = models.CharField(max_length=25)
-    productdescription = models.TextField(db_column='productDescription')  # Field name made lowercase.
-    quantityinstock = models.SmallIntegerField(db_column='quantityInStock')  # Field name made lowercase.
-    sellprice = models.IntegerField(db_column='sellPrice')  # Field name made lowercase.
-    buyprice = models.IntegerField(db_column='buyPrice')  # Field name made lowercase.
+    product_description = models.TextField(db_column='productDescription')  # Field name made lowercase.
+    quantity_in_stock = models.SmallIntegerField(db_column='quantityInStock')  # Field name made lowercase.
+    sell_price = models.IntegerField(db_column='sellPrice')  # Field name made lowercase.
+    buy_price = models.IntegerField(db_column='buyPrice')  # Field name made lowercase.
 
     class Meta:
         db_table = 'products'
--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
+"""Import models"""
 from django.shortcuts import render
-from .models import Products
+from .models import Product
 
 # Create your views here.
 
 
 def home(request):
-    pds = Products.objects.all()
+    """ Render product page """
+    pds = Product.objects.all()
     return render(request, 'product.html', {'products': pds})
+
+def product_details(request, pid):
+    product = Product.objects.get(product_id=pid)
+    return render(request, 'details.html', {'product': product})
