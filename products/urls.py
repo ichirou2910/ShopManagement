4e3cb05fc4c77c8681874702be203dcabe6c9c9e
thevsd <thevsd27@gmail.com>
2020-04-21 23:18:07 +0700

    embrace the change

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('viewproduct/<str:pid>/', views.product_details, name='details'),
-    path('inform/<str:pid>/', views.product_cart, name='cart')
+    path('<str:pid>/', views.product_cart, name='cart')
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,8 @@
 """Import models"""
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Product
-from .models import Cart
-from django.contrib.auth.models import User
+from .models import Product, Cart
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
@@ -24,7 +23,7 @@ def product_cart(request, pid):
     product_image = product_get.product_image
     quantity = 1
     price = product_get.sell_price
-    user = 3
+    user = 'none'
     product_in = Cart(product_id = product_id, product_name = product_name, quantity = quantity, price = price, user = user)
     product_in.save()
 
