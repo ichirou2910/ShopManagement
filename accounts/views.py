72b748d2d5021a57708117af3b07e68d0069a748
ichirou2910 <ichiroukeita2910@gmail.com>
2020-04-21 23:14:44 +0700

    Revamp login/register

--- accounts/views.py
+++ accounts/views.py
@@ -5,44 +5,48 @@ from django.contrib import messages
 # Create your views here.
 
 def register(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		email = request.POST['email']
-		password = request.POST['password']
-		password2 = request.POST['password2']
-		if password == password2:
-			if User.objects.filter(username = username).exists():
-				messages.info(request,'Username taken')
-				return redirect('register')
-			elif User.objects.filter(email = email).exists():
-				messages.info(request,'Email taken')
-				return redirect('register')
-			else:
-				user = User.objects.create_user(username = username, email = email, password = password)
-				user.save()
-				print('User created')
-				return redirect('login')
-		else:
-			messages.info(request,'Password not matching')
-			return redirect('register')
-		return redirect('/')
-	else:
-		return render(request, 'register.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            # If the username exists
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken, please use another one.')
+                return redirect('register')
+            # If the email exists
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken already, please use another one.')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                print('User created')
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not matching, please try again.')
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
+
 
 def login(request):
-	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		user = auth.authenticate(username = username, password = password)
-		if user is not None:
-			auth.login(request,user)
-			return redirect('/')
-		else:
-			messages.info(request,'Invalid credentials')
-			return redirect('login')
-	else:
-		return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials, please try again')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
 
 def logout(request):
-	auth.logout(request)
-	return redirect('/')
+    auth.logout(request)
+    return redirect('/')
