8069cd53bd74aaea3429316f7bbd4da8ceb9a88e
thevsd <thevsd27@gmail.com>
2020-04-24 16:16:57 +0700

    Init cart page

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
 ]
--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cart/', views.cart_get, name='cart_get'),
     path('viewproduct/<str:pid>/', views.product_details, name='details'),
     path('<str:pid>/', views.product_cart, name='cart'),
 ]
