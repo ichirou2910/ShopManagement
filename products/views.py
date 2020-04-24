7b05d7ceb45c8d89b531b0b41016bfac952e3431
thevsd <thevsd27@gmail.com>
2020-04-24 22:58:53 +0700

    Cart function is completed

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 """Import models"""
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db.models import Sum
 # from django.contrib.auth.models import User
 from .models import Product
 from .models import Cart
@@ -37,8 +38,8 @@ def product_cart(request, pid):
     product_get = Product.objects.get(product_id=pid)
     product_id = product_get.product_id
     product_name = product_get.product_name
-    # product_image = product_get.product_image
-    price = product_get.sell_price
+    product_image = product_get.product_image
+    price = product_get.sell_price * int(quantity)
 
     if quantity != 0 and user != "none":
         messages.add_message(request, messages.INFO, 'You have added a product to your cart!')
@@ -50,5 +51,5 @@ def product_cart(request, pid):
     return redirect('/products')
 
 def cart_get(request):
-    cart = Cart.objects.all()
+    cart = Cart.objects.filter(user = request.user.username).values('product_name', 'product_image', 'price', 'user').annotate(Sum('quantity'))
     return render(request, 'cart.html', {'cart': cart})
