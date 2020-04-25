2c14a7bc823c5052788b65ef76944bd4e30fb4a8
ichirou2910 <ichiroukeita2910@gmail.com>
2020-04-25 14:24:40 +0700

    Add limitation for add to cart function

--- products/views.py
+++ products/views.py
@@ -16,8 +16,12 @@ def home(request):
 
 
 def product_details(request, pid):
+    in_cart = Cart.objects.filter(user=request.user.username, product_id=pid).values('product_id', 'user').annotate(quantity=Sum('quantity'))
+    quantity = 0
+    if in_cart:
+        quantity = in_cart.get(product_id=pid).get('quantity')
     product = Product.objects.get(product_id=pid)
-    return render(request, 'details.html', {'product': product})
+    return render(request, 'details.html', {'product': product, 'quantity': quantity})
 
 
 def product_cart(request, pid):
@@ -38,13 +42,21 @@ def product_cart(request, pid):
     product_get = Product.objects.get(product_id=pid)
     product_id = product_get.product_id
     product_name = product_get.product_name
-    # product_image = product_get.product_image
+    product_stock = product_get.quantity_in_stock
     price = product_get.sell_price
 
+    in_cart = Cart.objects.filter(user=request.user.username, product_id=pid).values('product_id', 'user').annotate(quantity=Sum('quantity'))
+    cart_quantity = 0
+    if in_cart:
+        cart_quantity = in_cart.get(product_id=pid).get('quantity')
+
     if quantity != 0 and user != "none":
-        messages.add_message(request, messages.INFO, 'You have added a product to your cart!')
-        product_in = Cart(product_id=product_id, product_name=product_name, quantity=quantity, price=price, user=user)
-        product_in.save()
+        if int(quantity) + cart_quantity > product_stock:
+            messages.add_message(request, messages.INFO, "Heyyyy! You know we do not have that many right?")
+        else:
+            messages.add_message(request, messages.INFO, 'You have added a product to your cart!')
+            product_in = Cart(product_id=product_id, product_name=product_name, quantity=quantity, price=price, user=user)
+            product_in.save()
     else:
         messages.add_message(request, messages.INFO, 'Oops!')
 
@@ -52,7 +64,7 @@ def product_cart(request, pid):
 
 
 def cart_get(request):
-    cart = Cart.objects.filter(user=request.user.username).values('product_id', 'product_name', 'product_image', 'price', 'user').annotate(quantity = Sum('quantity'))
+    cart = Cart.objects.filter(user=request.user.username).values('product_id', 'product_name', 'product_image', 'price', 'user').annotate(quantity=Sum('quantity'))
     total_price = 0
     for cart_data in cart:
         total_price += cart_data.get('price') * cart_data.get('quantity')
